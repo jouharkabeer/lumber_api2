2b659c624c377be4a1e9f76dd4383d92693f7c63
Jouhar Kabeer <jouharkabeer412@gmail.com>
2025-08-11 09:40:20 +0530

    huj

--- lumber_api/settings.py
+++ lumber_api/settings.py
@@ -95,12 +95,12 @@ AUTH_USER_MODEL = 'master.User'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -161,16 +161,16 @@ if method == 'development':
 
     SECRET_KEY = 'django-insecure-)m(b&+41b#-x!zj#9hmo58jpv@qly$(q1+^n1r&ar2r+&hr3-v'
     DEBUG = True
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'railway',
-            'USER': 'postgres',
-            'PASSWORD': 'cZnrYFauUhfMTmTrOPHiFheoXymtuXIJ',
-            'HOST': 'trolley.proxy.rlwy.net',
-            'PORT': '16698',
-        }
-    }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'railway',
+    #         'USER': 'postgres',
+    #         'PASSWORD': 'cZnrYFauUhfMTmTrOPHiFheoXymtuXIJ',
+    #         'HOST': 'trolley.proxy.rlwy.net',
+    #         'PORT': '16698',
+    #     }
+    # }
 
 
 
