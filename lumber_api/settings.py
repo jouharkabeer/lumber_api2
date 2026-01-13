cf9b0cdcf504f3309a0ccf258911db7a2cbb29d2
jouharkabeer <jouharkabeer412@gmail.com>
2026-01-13 12:09:41 +0530

    hh

--- lumber_api/settings.py
+++ lumber_api/settings.py
@@ -40,7 +40,9 @@ ALLOWED_HOSTS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "http://127.0.0.1:5173",    
+    "http://localhost:8081",
+    "http://127.0.0.1:8081",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost",
